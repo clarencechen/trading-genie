d2d2b1aae191289d25cd6d427d279f2f945c574c
clarencechen <clarencechenct@gmail.com>
2016-11-12 04:31:30 -0800

    added node infra

--- main.js
+++ main.js
@@ -44,8 +44,9 @@ function setUpSocket() {
 					}
 					var req = http.request(options, function(res) {
 					    	console.log("In callback")
-					        console.log("body: " + res)
-							ws.send(res)
+					    	resstr = JSON.stringify(xmlToJson(res))
+					        console.log("body: " + resstr)
+							ws.send(resstr)
 					})
 					console.log('about to call nasdaq api')
 					req.end(data, 'utf8', function() {console.log('called api')})
@@ -87,4 +88,45 @@ function callQuery(data, callback) {
 function bail(err, callback) {
 	var error = {isError: true, error: err}
 	callback(error)
+}
+
+
+
+
+function xmlToJson(xml) {
+	
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
 }
