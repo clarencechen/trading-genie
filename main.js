b738a3e588d7f23e5c60ac0401380aaf40be69ab
clarencechen <clarencechenct@gmail.com>
2016-11-12 07:10:35 -0800

    added node infra

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ function setUpSocket() {
 					    	parseString(str, function (err, result) {
 								if(err)
 									console.log(JSON.stringify(err))
-								console.log(JSON.stringify(result.ArrayOfQuoteResults.QuoteResults.Quotes))
+								console.log(JSON.stringify(result.ArrayOfQuoteResults.QuoteResults))
 								string = result.ArrayOfQuoteResults.QuoteResults.Quotes.forEach(function(e, i) {
 									console.log('working on quote ' + i)
 									price[i] = +(e.BidPrice)
