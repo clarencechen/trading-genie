5ed37004beb4520450e38d4bc279e65c330c72be
clarencechen <clarencechenct@gmail.com>
2016-11-12 01:19:25 -0800

    added node infra

--- main.js
+++ main.js
@@ -8,9 +8,10 @@ var app = express()
 app.use('/', express.static(__dirname+"/static"))
 
 var port = (process.env.PORT || 5000);
-var server = http.createServer(app)
-server.listen(port)
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!')
+})
 
-app.post('/', function(req, res) {
+app.get('/', function(req, res) {
 	res.send('Hey')
 })
