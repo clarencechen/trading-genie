15096dd408485c3dc0a7f701817c63963df83aad
clarencechen <clarencechenct@gmail.com>
2016-11-12 06:14:20 -0800

    added node infra

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ console.log('app started')
 var querystring = require('querystring')
 var express = require('express')
 var http = require('http')
-var parseString = require('xml2js').parseString;
+var parseString = require('xml2js').parseString
 var app = express()
 
 app.use('/', express.static(__dirname+"/static"))
-
 var port = (process.env.PORT || 5000);
 var server = http.createServer(app)
 server.listen(port)
@@ -98,45 +97,4 @@ function callQuery(data, callback) {
 function bail(err, callback) {
 	var error = {isError: true, error: err}
 	callback(error)
-}
-
-
-
-
-function xmlToJson(xml) {
-	
-	// Create the return object
-	var obj = {};
-
-	if (xml.nodeType == 1) { // element
-		// do attributes
-		if (xml.attributes.length > 0) {
-		obj["@attributes"] = {};
-			for (var j = 0; j < xml.attributes.length; j++) {
-				var attribute = xml.attributes.item(j);
-				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-			}
-		}
-	} else if (xml.nodeType == 3) { // text
-		obj = xml.nodeValue;
-	}
-
-	// do children
-	if (xml.hasChildNodes()) {
-		for(var i = 0; i < xml.childNodes.length; i++) {
-			var item = xml.childNodes.item(i);
-			var nodeName = item.nodeName;
-			if (typeof(obj[nodeName]) == "undefined") {
-				obj[nodeName] = xmlToJson(item);
-			} else {
-				if (typeof(obj[nodeName].push) == "undefined") {
-					var old = obj[nodeName];
-					obj[nodeName] = [];
-					obj[nodeName].push(old);
-				}
-				obj[nodeName].push(xmlToJson(item));
-			}
-		}
-	}
-	return obj;
 }
