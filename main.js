72e2372339284afb370226f8c02c6661cc177c0d
clarencechen <clarencechenct@gmail.com>
2016-11-12 02:58:25 -0800

    added node infra

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-console.log('app started')
 
+console.log('app started')
+var querystring = require('querystring')
 var express = require('express')
 var http = require('http')
 
@@ -8,10 +9,85 @@ var app = express()
 app.use('/', express.static(__dirname+"/static"))
 
 var port = (process.env.PORT || 5000);
-app.listen(port, function () {
-  console.log('Example app listening on port 3000!')
-})
+var server = http.createServer(app)
+server.listen(port)
 
-app.post('/static/index.html', function(req, res) {
-	res.send('Hey')
-})
+var WebSocket = require("ws")
+var ws = new WebSocket('wss://trading-genie.herokuapp.com/')
+var WebSocketServer = require("ws").Server
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+function setUpSocket() {
+	wss.on("connection", function(ws) {
+		console.log("websocket connection open")
+		ws.on("message", function(data) {//data from webpage
+			console.log('received data')
+			var stuff = JSON.parse(data)
+			var id = stuff[0]
+			var symbols = stuff['symbols']
+			switch (id) {
+				case "quotes":
+				{
+					var data = querystring.stringify({	'_Token' : '999AEED3D3454B599104F310E15B1CD4',
+										'Symbols' : stuff['symbols'],
+										'StartDateTime' : stuff['start'],
+										'EndDateTime' : stuff['end'],
+										'MarketCenters' : '' 
+									})
+					var options = {
+						host: 'http://ws.nasdaqdod.com',
+						port: 80,
+						path: '/v1/NASDAQQuotes.asmx/GetQuotes',
+						method: 'POST',
+						headers: {
+						    'Content-Type': 'application/x-www-form-urlencoded',
+						    'Content-Length': Buffer.byteLength(JSON.stringify(data), 'utf8')
+						}
+					}
+					console.log('about to call nasdaq api')
+					var req = http.request(options, function(res) {
+					    res.on('data', function (resp) {
+					    	console.log("In callback")
+					        console.log("body: " + resp)
+							ws.send(resp)
+					    })
+					})
+					req.write(data)
+					req.end()
+					break
+				}
+				default:
+				{
+					console.log('crap')
+				}
+			}
+		});
+	
+		ws.on("close", function() {
+			console.log("websocket connection close")
+		})
+	})
+}
+
+setUpSocket()
+
+function callQuery(data, callback) {
+	client.post('analyzesentiment', data, function(err, resp) {
+		if(err)
+		{
+			console.log('An error occured! ' + err)
+			bail(err, callback)
+		}
+		else
+		{
+			console.log('We got ' + JSON.stringify(resp.body))
+			callback(resp.body);
+		}
+	})
+}
+
+function bail(err, callback) {
+	var error = {isError: true, error: err}
+	callback(error)
+}
---
+++ static/index.js
@@ -0,0 +1,25 @@
+var host = location.origin.replace(/^http/, 'ws')
+var ws = new WebSocket(host);
+$(document).ready(function() {
+	$("button#submit").click(submitQuery);
+	$("#comment").keypress(function(e){
+		if(e.which == 13) {
+			e.preventDefault()
+			submitQuery()
+		}
+	})
+	ws.onmessage = function(data){
+		var obj = JSON.parse(data)
+		else
+			$('#review').append('<p class="review">' + data + '</p>')
+	}
+})
+function submitQuery() {
+	if($("#comment").val() == "")
+		return "";
+	var query = {symbols: 'AAPL', start: '2/1/2015 00:00:00.000', end: '2/5/2015 00:00:00.000'}
+	//$("#comment").val();
+	$("#comment").val("");
+	ws.send("quote::" + query);
+	console.log("emitted " + query);
+}
