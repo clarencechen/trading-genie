28414531d390341240ca6c002ef0013fb067340a
clarencechen <clarencechenct@gmail.com>
2016-11-13 01:56:45 -0800

    added node infra

--- static/index.js
+++ static/index.js
@@ -14,42 +14,28 @@ $(document).ready(function() {
 	})
 	ws.onmessage = function(event){
 		if(event.data == 'ping')
-		{
 			console.log('staying alive')
-		}
 		else if(event.data == 'pong')
-		{
 			console.log('still staying alive')	
-		}
 		else if(event.data.split("::")[0] == 'stock')
-		{
 			stock = event.data.split("::")[1]	
-		}
 		else if(event.data.split("::")[0] == 'lavg')
-		{
 			lavg = JSON.parse(event.data.split("::")[1])
-			$('#lavg').text(JSON.stringify(lavg))
-		}
 		else if(event.data.split("::")[0] == 'havg')
-		{
 			havg = JSON.parse(event.data.split("::")[1])
-			$('#havg').text(JSON.stringify(havg))
-		}
 		else if(event.data.split("::")[0] == 'profit')
 		{
 			profit = +(event.data.split("::")[1])
-			console.log('profit: ' + profit)
+			('#profit').append('<h2>Total Profit: ' + profit + '</h2>')
 		}
 		else if(event.data.split("::")[0] == 'price')
-		{
 			$.extend(price, JSON.parse(event.data.split("::")[1]))
-		}
 		else if(event.data = 'end')
 		{
 			var bottom = d3.min([d3.min(lavg), d3.min(price), d3.min(havg)])
 			var top = d3.max([d3.max(lavg), d3.max(price), d3.max(havg)])
 			console.log(top + ' ' +bottom);
-			$('#charttitle').text('<h2>Strategy results for ' + stock + '</h2>')
+			$('#charttitle').append('<h2>Strategy results for ' + stock + '</h2>')
 			plot(lavg, "blue", "Low Moving Average", top, bottom);
 			plot(havg, "red", "High Moving Average", top, bottom);
 			plot(price, "green", "Instantaneos Price", top, bottom);
@@ -98,37 +84,37 @@ function setupSVG() {
 	
 	var xAxis = d3.axisBottom()
 			.scale(x).ticks(0);
-}
 
-function plot(data, color, name, top, bottom) {
-	x.domain([0, data.length]);
-	y.domain([bottom,top]);
-
-	g.append("g")
-			.attr("class", "xaxis")
-			.attr("transform", "translate(0," + height + ")")
-			.call(xAxis);
-
-	g.append("g")
-			.attr("class", "yaxis")
-			.call(d3.axisLeft(y))
-			.append("text")
-			.attr("fill", "#333")
-			.attr("transform", "rotate(-90)")
-			.attr("y", 6)
-			.attr("dy", "0.71em")
-			.style("text-anchor", "end")
-			.text("Price ($)");
-
-	g.append("path")
-			.attr("class", "line")
-			.attr("stroke",color)
-			.attr("d", line(data));
-
-	svg.append("text")
-			.attr("transform", "translate(" + (width+10) + "," + y(data[data.length-1].value-20) + ")")
-			.attr("dy", ".35em")
-			.attr("text-anchor", "start")
-			.style("fill", color)
-			.text(name);
+	function plot(data, color, name, top, bottom) {
+		x.domain([0, data.length]);
+		y.domain([bottom,top]);
+	
+		g.append("g")
+				.attr("class", "xaxis")
+				.attr("transform", "translate(0," + height + ")")
+				.call(xAxis);
+	
+		g.append("g")
+				.attr("class", "yaxis")
+				.call(d3.axisLeft(y))
+				.append("text")
+				.attr("fill", "#333")
+				.attr("transform", "rotate(-90)")
+				.attr("y", 6)
+				.attr("dy", "0.71em")
+				.style("text-anchor", "end")
+				.text("Price ($)");
+	
+		g.append("path")
+				.attr("class", "line")
+				.attr("stroke",color)
+				.attr("d", line(data));
+	
+		svg.append("text")
+				.attr("transform", "translate(" + (width+10) + "," + y(data[data.length-1].value-20) + ")")
+				.attr("dy", ".35em")
+				.attr("text-anchor", "start")
+				.style("fill", color)
+				.text(name);
+	}
 }
