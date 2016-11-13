496930b0c2afeb50a18ca9f066cac4c74c07fd8b
clarencechen <clarencechenct@gmail.com>
2016-11-12 17:16:48 -0800

    added node infra

--- main.js
+++ main.js
@@ -106,8 +106,8 @@ function setUpSocket() {
 
 							var lavg, havg = []
 							for(i = 1; i <= Module.getValue(LMApointer, 'double'); i++)
-								console.log(lavg[i])
 								lavg[i -1] = Module.getValue(LMApointer +8*i, 'double')
+								console.log(lavg[i -1])
 							for(i = 1; i <= Module.getValue(HMApointer, 'double'); i++)
 								havg[i -1] = Module.getValue(HMApointer +8*i, 'double')
 							ws.send("lavg::" +JSON.stringify(lavg))
--- static/index.js
+++ static/index.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
 					})
 				}
 			})
-			arr = [{[]},{[]}]
+			arr = [{},{}]
 		}
 		else
 		{
